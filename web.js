4113bf48f5ec1a769006fe88a73b3801617cc67d
naoki.otsu <naoki.otsu@dena.jp>
2015-02-06 15:22:22 +0900

    test mobile bug

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var express = require('express');
 	app = express(),
 	server = require('http').createServer(app),
-	path = require('path');
+	path = require('path'),
+	io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 8000);
 
@@ -14,27 +15,27 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-var io = require('socket.io').listen(server);
-
 // Herokuは下記を入れないとSocket.ioが動かない情報がWebにあったが、なくても動くもよう(WebSocketに対応されたからか)
-// io.configure(function () { 
-//   io.set("transports", ["xhr-polling"]); 
-//   io.set("polling duration", 10); 
-// });
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
 
 // io.set('log level', 1); // 自動で出るデバック情報を出さない(ローカルでデバックする際に使用)
 
 // クライアント(index.html)からの接続を監視
-// io.sockets.on("connection", function (socket) {
-// 	// クライアントから "emit_from_client" が送られてきたら
-// 	socket.on("emit_from_client", function (id) {
-// 		// 接続しているソケットのみにidを送信する
-// 		// socket.emit("emit_from_server", id);
+io.sockets.on("connection", function (socket) {
+	// クライアントから "emit_from_client" が送られてきたら
+	socket.on("emit_from_client", function (id) {
+		// console.log(id);
 		
-// 		// 接続しているソケット以外全てにidを送信する
-// 		//socket.broadcast.emit("emit_from_server", id);
+		// 接続しているソケットのみにidを送信する
+		// socket.emit("emit_from_server", id);
 		
-// 		// 接続しているソケット含めて全部にidを送信する
-// 		io.sockets.emit("emit_from_server", id);
-// 	});
-// });
+		// 接続しているソケット以外全てにidを送信する
+		//socket.broadcast.emit("emit_from_server", id);
+		
+		// 接続しているソケット含めて全部にidを送信する
+		io.sockets.emit("emit_from_server", id);
+	});
+});
