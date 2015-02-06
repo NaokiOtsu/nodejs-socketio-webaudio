0dfec866d967e91518a693559e7b1cf9870b046c
naoki.otsu <naoki.otsu@dena.jp>
2015-02-06 13:44:02 +0900

    test mobile bug

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 	app = express(),
 	server = require('http').createServer(app),
-	path = require('path');
-	// io = require('socket.io').listen(server);
+	path = require('path'),
+	io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 8000);
 
